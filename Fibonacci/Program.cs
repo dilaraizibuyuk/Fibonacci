7f3e6ee778697bb154eec52b80a3e6f88b9e2212
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-10 17:43:47 +0300

    Proje dosyası ekle.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 1;
+            int sayi;
+
+            Console.WriteLine("Fibonacci dizisi kaç elemanlı olsun?");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("===========================================");
+            Console.WriteLine(x+" "+y+" ");
+            for (int i = 0; i <=sayi ; i++)
+            {
+                int z = x + y;
+                Console.WriteLine(z+" ");
+                x = y;
+                y = z;
+
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
+ 
